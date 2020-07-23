5ec0e142b80b737137f5f548b088a333d62477eb
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-07-24 01:37:10 +0300

    Delete closeBtn.js

--- New/4 ToDo/SCRIPTS/closeBtn.js
+++
@@ -1,8 +0,0 @@
-var closeBtn = document.querySelector(".closebtn");
-
-export function closing(){
-    closeBtn.addEventListener("click", function(event){
-        event.preventDefault();
-        overlay.style.height ="0" ;
-    });
-}
