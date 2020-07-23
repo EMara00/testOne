d5983511b4d7126f22491b841e96628b57c460ea
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-07-24 01:39:55 +0300

    Delete tipsBtn.js

--- New/4 ToDo/SCRIPTS/tipsBtn.js
+++
@@ -1,8 +0,0 @@
-var overlay = document.getElementById("overlay");
-var tipsBtn = document.querySelector(".tipBtn");
-
-export function tipsing(){
-    tipsBtn.addEventListener("click", function(){
-        overlay.style.height ="100%" ;
-    });
-}
